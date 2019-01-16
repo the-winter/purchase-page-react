18e3abc14fb663413605e6e50b84357052d96f21
thewinter <19483860+the-winter@users.noreply.github.com>
2019-01-16 15:18:31 +0800

    created build for gh pages

---
+++ build/precache-manifest.fd5a8a147f82aff4e9ae685a5381c4df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2325768f58d7e9120e796fa181ba6f1",
+    "url": "/static/media/left-arrow.a2325768.svg"
+  },
+  {
+    "revision": "cbde25c1895bb06ea2bf5bd8272c84ee",
+    "url": "/static/media/ath-msr7-brown.cbde25c1.jpg"
+  },
+  {
+    "revision": "eb57ad5cdf4e1083358e0b53b17a9c82",
+    "url": "/static/media/ath-msr7-black.eb57ad5c.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "622c58471294e9633769",
+    "url": "/static/js/main.622c5847.chunk.js"
+  },
+  {
+    "revision": "938f4def128bfb6b6972",
+    "url": "/static/js/1.938f4def.chunk.js"
+  },
+  {
+    "revision": "622c58471294e9633769",
+    "url": "/static/css/main.9cd6d0fe.chunk.css"
+  },
+  {
+    "revision": "90e4737ea56d01fa9976c5a13d17b6be",
+    "url": "/index.html"
+  }
+];
